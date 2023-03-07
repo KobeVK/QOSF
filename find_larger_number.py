ad16ebe4324bdc94cfb93278ea2064af90320d4e
Kobe Vaknin <skvaknin@gmail.com>
2023-03-07 22:32:27 +0200

    Finalizing script

--- find_larger_number.py
+++ find_larger_number.py
@@ -1,60 +1,81 @@
-from math import ceil, log2 
-from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, execute, Aer
-import sys
+# Importing necessary libraries
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit, Aer, execute
 from qiskit.visualization import *
-from matplotlib import style
-from sys import argv
 
-int1 = 100
-int2 = 102123
+# Implementing the Bernstein-Vazirani Oracle
+def bv_circuit(a, b):
+    """
+    This function creates a quantum circuit that can determine the larger of two numbers using the Bernstein-Vazirani algorithm.
+    It takes two integer inputs a and b, and returns the corresponding Bernstein-Vazirani Oracle quantum circuit.
+    """
+    # Marking the solution
+    A = int(a/max(a, b))
+    B = int(b/max(a, b))
 
-def compare_integers(A, B):
-
-    # Determine the number of qubits required to represent A and B
-    num_qubits = ceil(log2(max(A, B))) + 1
-    print(f"number of Qbits required:", num_qubits) 
-            
-    # Create a quantum circuit with qubits required and one classical bit
-    qc = QuantumCircuit(num_qubits, 1)
+    # Creating the base circuit
+    q = QuantumRegister(3, "q") 
+    c = ClassicalRegister(2, "c")
+    bvqc = QuantumCircuit(q, c)
     
-    # Create a superposition of all possible values of A and B using a Hadamard gate on each qubit
-    for i in range(num_qubits):
-        qc.h(i)
+    # Applying the Hadamard's sandwich
+    bvqc.x(2)
+    bvqc.barrier()
+    bvqc.h(0)
+    bvqc.h(1)
+    bvqc.h(2)
     
-    # Define the oracle that marks the state where A equal to B
-    for i in range(num_qubits):
-        qc.x(i)
-        qc.cz(num_qubits - 1, 0)
-    for i in range(num_qubits):
-        qc.x(i)
+    # Applying the Oracle
+    if(A == 1):
+        bvqc.cx(0, 2)
+    elif(B == 1):
+        bvqc.cx(1, 2)
 
+    # Closing the Hadamard's sandwich
+    bvqc.h(0)
+    bvqc.h(1)
+    bvqc.h(2)
+    bvqc.barrier()
 
-    # Apply Grover's algorithm amplitude amplification procedure to the superposition to amplify the marked state
-    num_iterations = int(round((2 ** num_qubits) ** 0.5)) - 1
-    for i in range(num_iterations):
-        qc.h(range(num_qubits))
-        for j in range(num_qubits):
-            qc.x(j)
-        qc.h(num_qubits-1)
-        qc.mct(list(range(num_qubits-1)), num_qubits-1)
-        qc.h(num_qubits-1)
-        for j in range(num_qubits):
-            qc.x(j)
-        qc.h(range(num_qubits)) 
+    # Measuring the first two qubits and storing the result in the classical register
+    bvqc.measure([0, 1],[0, 1])
 
-    # Measure the output qubit
-    qc.measure(num_qubits-1, 0)
-    # Run the circuit on a simulator
-    backend = Aer.get_backend('qasm_simulator')
-    shots = 1024
-    result = execute(qc, backend, shots=shots).result()
-    counts = result.get_counts()
-    circuit_diagram = qc.draw(output='mpl')
-    return counts, circuit_diagram
+    return bvqc
 
+def find_the_largest_number(a, b):
+    """
+    This function finds the larger of two numbers using the Bernstein-Vazirani algorithm.
+    It takes two integer inputs a and b, and returns the larger of the two numbers along with the circuit visualization and a histogram of the measurement results.
+    """
+    # Building the Bernstein-Vazirani circuit for the given input
+    qc = bv_circuit(a, b)
+    
+    # backend for simulation
+    aer = Aer.get_backend('aer_simulator')
+    
+    # Executing the circuit and obtaining the counts
+    counts = execute(qc, backend = aer, shots = 1024).result().get_counts()
+    
+    # Determining the most frequently occurring result
+    most_frequent_result = counts.most_frequent()
+    
+    # Creating a list with the input numbers and returning the larger number based on the measured output
+    num_list = [a, b]
+    result = num_list[int(most_frequent_result, 2)-1]
+    
+    # Drawing the circuit and plotting a histogram of the counts
+    circuit_visualization = qc.draw(output='mpl')
+    histogram = plot_histogram(counts)
+    
+    return result, circuit_visualization, histogram
 
-count, img = compare_integers(int1, int2)
-if '0' in count:
-    print(f"The larger integer is {int1}.")
-else:
-    print(f"The larger integer is {int2}.")
+# Finding the larger of two numbers (7 and -4) using the Bernstein-Vazirani algorithm
+result, circuit_visualization, histogram = find_the_largest_number(7, -4)
+
+# Printing the result
+print("The larger of the two numbers is: ", result)
+
+# Displaying the circuit visualization
+circuit_visualization
+
+# Displaying the histogram
+# histogram
